d3284ee04fc9060dad08b76cb83dcd459f41b4fa
zxcdr2 <zxcdr2@sina.com>
2020-09-25 17:44:29 +0800

    增加方法

--- src/main/java/cn/infocore/manager/MetaManager.java
+++ src/main/java/cn/infocore/manager/MetaManager.java
@@ -1,5 +1,6 @@
 package cn.infocore.manager;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -8,10 +9,24 @@ import cn.infocore.dao.EcsMapper;
 import cn.infocore.dao.MdbMapper;
 import cn.infocore.entity.EcsDO;
 import cn.infocore.entity.MdbDO;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 @Service
 public class MetaManager extends ServiceImpl<MdbMapper, MdbDO> {
 
-    
-    
-    
+    public List<String> getMetaUserIdsById(String id) {
+        LambdaQueryWrapper<MdbDO> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(MdbDO::getId, id);
+        LinkedList<String> userIdList = new LinkedList<>();
+
+        Collection<MdbDO> mdbDOS = this.list(lambdaQueryWrapper);
+        for (MdbDO mdbDO : mdbDOS) {
+            userIdList.add(mdbDO.getUserId());
+        }
+
+        return userIdList;
+    }
 }
