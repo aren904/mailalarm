95a7c634adb746b025dacc2fc838f6152d68c6a1
zxcdr2 <zxcdr2@sina.com>
2020-10-27 17:25:24 +0800

    加入rds相关告警方法

--- src/main/java/cn/infocore/service/RDSService.java
+++ src/main/java/cn/infocore/service/RDSService.java
@@ -2,6 +2,7 @@ package cn.infocore.service;
 
 import java.util.List;
 
+import cn.infocore.bo.FaultSimple;
 import cn.infocore.dto.DataArkDTO;
 import cn.infocore.entity.Fault;
 import cn.infocore.entity.RdsDO;
@@ -15,4 +16,6 @@ public interface RDSService {
 	List<Fault> getFault(DataArkDTO data_ark, List<RdsInfo> rdsInfoList);
 	
 	List<RdsInstanceDO> getRDSInstanceListFromSource(DataArkDTO data_ark, List<RdsInfo> rdsInfoList);
+
+	List<FaultSimple> updateRdsInfoClientList(List<RdsInfo> rdsInfo);
 }
