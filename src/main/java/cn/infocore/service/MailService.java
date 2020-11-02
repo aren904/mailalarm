d79833d3fff2623720efde0a288a0de4aab34223
zxcdr2 <zxcdr2@sina.com>
2020-11-02 10:24:15 +0800

    修改接口

--- src/main/java/cn/infocore/service/MailService.java
+++ src/main/java/cn/infocore/service/MailService.java
@@ -23,7 +23,10 @@ public interface MailService {
 	void updateMailService(String name,Email_alarm sender);
 	
 	//通知
-	void notifyCenter (DataArkDTO data_ark,List<Client_> clientList,List<Vcenter> vcList,List<Virtual_machine> vmList,List<RdsDO> rdsList, List<RdsInstanceDO> rdsInstances,List<Fault> list_fault) throws SQLException;
+//	void notifyCenter (DataArkDTO data_ark,List<Client_> clientList,List<Vcenter> vcList,List<Virtual_machine> vmList,List<RdsDO> rdsList, List<RdsInstanceDO> rdsInstances,List<Fault> list_fault) throws SQLException;
+
+	void notifyCenter (DataArkDTO data_ark,List<Client_> clientList,List<Vcenter> vcList,List<Virtual_machine> vmList,List<Fault> list_fault) throws SQLException;
+
 
 	void sentFault(Collection<FaultSimple> faultSimples);
 }
