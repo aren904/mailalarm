56b5c022262d30b14c5deb85b1729aa63f935fe7
zxcdr2 <zxcdr2@sina.com>
2020-12-24 14:02:19 +0800

    协议更改，增加错误码

--- src/main/java/cn/infocore/App.java
+++ src/main/java/cn/infocore/App.java
@@ -6,6 +6,7 @@ package cn.infocore;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import cn.infocore.main.MailMain;
@@ -21,9 +22,6 @@ public class App {
 		ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
 		MailMain mainRunner = context.getBean(MailMain.class);
 		mainRunner.start();
-
-
-		
 	}
 
 }
--- src/main/java/cn/infocore/main/ThreadInformation.java
+++ src/main/java/cn/infocore/main/ThreadInformation.java
@@ -11,6 +11,7 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 //接收来自强哥的通知
+
 @Component
 public class ThreadInformation extends Thread {
     private static final Logger logger = Logger.getLogger(ThreadInformation.class);
